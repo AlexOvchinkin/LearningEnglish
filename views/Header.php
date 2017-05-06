f8972a5838067674ea5baf1aaa5c7f4ef6517bdf
AlexOvchinkin <al.ovchinkin@gmail.com>
2017-05-06 18:21:11 +0300

    06-05-2017 (2)

--- views/Header.php
+++ views/Header.php
@@ -8,10 +8,10 @@
 <body>
 <header class="block-header">
     <div class="block-header__left-part">
-        <img class="block-header__logo-picture" src="../src/image/rocket.png">
-        <div class="block-header__logo-text">
-            Rocket ENGLISH
-        </div>
+        <a class="block-header__logo-link" href="/">
+            <img class="block-header__logo-picture" src="../src/image/rocket.png">
+        </a>
+        <a class="block-header__logo-text-link" href="/">Rocket ENGLISH</a>
         <a class="block-header__link  block-header__link-lessons" href="#">
             УРОКИ
         </a>
